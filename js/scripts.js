aa57b26b2844e150e6c486f70f4a8511926b257d
unknown <1163751581@qq.com>
2016-08-28 21:38:20 +0800

    移动业务

--- js/scripts.js
+++ js/scripts.js
@@ -33,6 +33,24 @@ $(function(){
         }
     });
 
+    //信息弹窗
+    $('.add-addr-info').click(function(){
+        $('.ui-dialog').addClass('show');
+        $('.ui-dialog').find('.icon-error').click(function(){
+            $('.ui-dialog').removeClass('show');
+        })
+    });
+
+    window.onload=function(){
+        // document.getElementById("left").style.height = document.getElementById("right").offsetHeight+"px";
+       // $('.left-nav-2').height($('.charge-container').get(0).offsetHeight )
+       var hl = $(".left-nav-2").outerHeight(); //获取左侧left层的高度 
+       var hr = $(".charge-container").outerHeight(); //获取右侧right层的高度  
+       var mh = Math.max(hl,hr); //比较hl与hr的高度，并将最大值赋给变量mh
+       $(".left-nav-2").height(mh); //将left层高度设为最大高度mh  
+       $(".charge-container").height(mh); //将right层高度设为最大高度
+    }
+
 })
 
 // -----------------返回顶部-----------------
